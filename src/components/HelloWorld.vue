bae6ef3b3fe799b9c53cc15009daeb9cd8b50fe0
jianglin <843505141@qq.com>
2018-10-18 10:36:34 +0800

    添加动态数据

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,71 +29,6 @@ export default {
           date: '时间',
           other: '其他信息'
         }
-      },{
-        src: 'https://www.291buy.cn/datelist/img/60.png',
-        title: '标题二',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: {
-          path: '/chakan',
-          replace: false
-        },
-        meta: {
-          source: '来源信息',
-          date: '时间',
-          other: '其他信息'
-        }
-      },{
-        src: 'https://www.291buy.cn/datelist/img/60.png',
-        title: '标题二',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: {
-          path: '/chakan',
-          replace: false
-        },
-        meta: {
-          source: '来源信息',
-          date: '时间',
-          other: '其他信息'
-        }
-      },{
-        src: 'https://www.291buy.cn/datelist/img/60.png',
-        title: '标题二',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: {
-          path: '/chakan',
-          replace: false
-        },
-        meta: {
-          source: '来源信息',
-          date: '时间',
-          other: '其他信息'
-        }
-      },{
-        src: 'https://www.291buy.cn/datelist/img/60.png',
-        title: '标题二',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: {
-          path: '/chakan',
-          replace: false
-        },
-        meta: {
-          source: '来源信息',
-          date: '时间',
-          other: '其他信息'
-        }
-      },{
-        src: 'https://www.291buy.cn/datelist/img/60.png',
-        title: '标题二',
-        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
-        url: {
-          path: '/chakan',
-          replace: false
-        },
-        meta: {
-          source: '来源信息',
-          date: '时间',
-          other: '其他信息'
-        }
       }],
       footer: {
         title: 'more',
@@ -105,6 +40,17 @@ export default {
     onImgError (item, $event) {
       console.log(item, $event)
     }
+  },
+  created() {
+    axios.post('https://www.291buy.cn/datelist/php/res.php', qs.stringify({
+      type: 'getRes'
+      }))
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 }
 </script>
